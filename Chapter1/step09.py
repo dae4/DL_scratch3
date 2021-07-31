017be134e211c23929989ebebedeb286e7ed2776
dae4 <3210m0123@gmail.com>
2021-07-31 16:34:00 +0900

    code simplification

--- Chapter1/step09.py
+++ Chapter1/step09.py
@@ -1,8 +1,12 @@
+#%%
 import numpy as np
 
 
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError("{}은 지원하지 않음".format(type(data)))
         self.data = data
         self.grad = None
         self.creator = None
@@ -11,6 +15,9 @@ class Variable:
         self.creator = func
 
     def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
         funcs=[self.creator]
         while funcs:
             f = funcs.pop()
@@ -20,11 +27,16 @@ class Variable:
             if x.creator is not None:
                 funcs.append(x.creator)
 
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
 class Function:
     def  __call__(self, input):
         x = input.data
         y = self.forward(x)
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self)
         self.input = input
         self.output = output
@@ -57,14 +69,8 @@ def square(x):
 def exp(x):
     return Exp()(x)
 
+#%%
 x = Variable(np.array(0.5))
-
-a = Square()(x)
-b = Exp()(a)
-y = Square()(b)
-
-y.grad = np.array(1.0)
-
+y = square(exp(square(x)))
 y.backward()
 print(x.grad)
-
