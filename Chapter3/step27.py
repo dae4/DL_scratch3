b536f3819f610818d7b83fd255e01170d410cf87
dae4 <3210m0123@gmail.com>
2021-08-18 16:50:41 +0900

    update my sin function

--- Chapter3/step27.py
+++ Chapter3/step27.py
@@ -18,3 +18,33 @@ class Sin(Function):
         
 def sin(x):
     return Sin()(x)
+
+from dezero import Variable
+
+x = Variable(np.array(np.pi/4))
+y = sin(x)
+y.backward()
+
+print(y.data)
+print(x.grad)
+# %%
+import math 
+def my_sin(x, threshold = 0.0001):
+    y=0 
+    for i in range(10000):
+        c = (-1) ** i / math.factorial(2 * i + 1)
+        t = c * x ** (2 * i + 1)
+        y = y +t
+        if abs(t.data) < threshold :
+            break
+
+    return y
+
+x = Variable(np.array(np.pi/4))
+y = sin(x)
+y.backward()
+
+print(y.data)
+print(x.grad)
+
+# %%
