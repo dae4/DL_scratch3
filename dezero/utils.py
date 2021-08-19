b50ae450fafbff5303f889d52f040e16a7a969c7
dae4 <3210m0123@gmail.com>
2021-08-19 21:56:02 +0900

    update utils

--- dezero/utils.py
+++ dezero/utils.py
@@ -1,51 +1,59 @@
 #%%
 import os 
 import subprocess
-
-def _dot_var(v,verbose=False):
+def _dot_var(v, verbose=False):
     dot_var = '{} [label="{}", color=orange, style=filled]\n'
 
     name = '' if v.name is None else v.name
-    if verbose and v.data is not None :
+    if verbose and v.data is not None:
         if v.name is not None:
-            name+= ': '
-        name += str(v.shape) + ' ' +str(v.dtype)
-    return dot_var.format(id(v),name)
+            name += ': '
+        name += str(v.shape) + ' ' + str(v.dtype)
+
+    return dot_var.format(id(v), name)
+
 
 def _dot_func(f):
+    # for function
     dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
-    txt = dot_func.format(id(f),f.__class__.__name__)
+    ret = dot_func.format(id(f), f.__class__.__name__)
 
+    # for edge
     dot_edge = '{} -> {}\n'
     for x in f.inputs:
-        txt += dot_edge.format(id(x),id(f))
-    for y in f.outputs:
-        txt += dot_edge.format(id(f),id(y()))
-    return txt
+        ret += dot_edge.format(id(x), id(f))
+    for y in f.outputs:  # y is weakref
+        ret += dot_edge.format(id(f), id(y()))
+    return ret
 
-def get_dot_graph(output,verbose=True):
+
+def get_dot_graph(output, verbose=True):
     txt = ''
-    funcs=[]
-    seen_set=set()
+    funcs = []
+    seen_set = set()
 
     def add_func(f):
         if f not in seen_set:
             funcs.append(f)
-            #funcs.sort(key=lambda x:x.generation)
+            # funcs.sort(key=lambda x: x.generation)
             seen_set.add(f)
-            
+
     add_func(output.creator)
-    txt+=_dot_var(output, verbose)
+    txt += _dot_var(output, verbose)
+
     while funcs:
         func = funcs.pop()
-        txt = _dot_func(func)
+        txt += _dot_func(func)
         for x in func.inputs:
-            txt+=_dot_var(x,verbose)
+            txt += _dot_var(x, verbose)
+
             if x.creator is not None:
                 add_func(x.creator)
-    return 'digraph g {\n' +txt +'}'
 
-def plot_dot_graph(output, verbose=True,to_file='graph.png'):
+    return 'digraph g {\n' + txt + '}'
+
+
+def plot_dot_graph(output, verbose=True, to_file='graph.png'):
     dot_graph = get_dot_graph(output, verbose)
 
     tmp_dir = os.path.join(os.path.expanduser('~'), '.dezero')
