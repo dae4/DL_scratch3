3b44e1e7eccf13ebce4641b47512b57ce4f8497c
dae4 <3210m0123@gmail.com>
2021-08-24 21:16:51 +0900

    Neural Network

--- dezero/functions.py
+++ dezero/functions.py
@@ -53,6 +53,20 @@ class Reshape(Function):
     def backward(self, gy):
         return reshape(gy,self.x_shape)
 
+class Exp(Function):
+    def forward(self,x):
+        y = np.exp(x)
+        return y
+
+    def backward(self,gy):
+        y = self.outputs[0]()  # weakref
+        gx = gy * y
+        return gx
+
+def exp(x):
+    return Exp()(x)
+
+
 def reshape(x,shape):
     if x.shape == shape:
         return as_variable(x)
@@ -154,3 +168,52 @@ class MeanSquaredError(Function):
     
 def mean_squared_error(x0,x1):
     return MeanSquaredError()(x0,x1)
+
+
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+def linear_simple(x, W, b=None):
+    t = matmul(x, W)
+    if b is None:
+        return t
+
+    y = t + b
+    t.data = None  # Release t.data (ndarray) for memory efficiency
+    return y
+
+class Sigmoid(Function):
+    def forward(self, x):
+        # y = 1 / (1 + xp.exp(-x))
+        y = tanh(x * 0.5) * 0.5 + 0.5  # Better implementation
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
+
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+    return y
