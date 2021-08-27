8d0036661ca027cc336ddebd9379039a8aab627c
dae4 <3210m0123@gmail.com>
2021-08-27 16:37:12 +0900

    fix error

--- Chapter4/step45.py
+++ Chapter4/step45.py
@@ -9,20 +9,20 @@ import dezero.functions as F
 import dezero.layers as L
 from dezero import Model 
 
-class TwoLayerNet(Model):
-    def __init__(self, hidden_size,out_size):
-        super().__init__()
-        self.l1 = L.Linear(hidden_size)
-        self.l2 = L.Linear(out_size)
+# class TwoLayerNet(Model):
+#     def __init__(self, hidden_size,out_size):
+#         super().__init__()
+#         self.l1 = L.Linear(hidden_size)
+#         self.l2 = L.Linear(out_size)
 
-    def forward(self, x):
-        y = F.sigmoid(self.l1(x))
-        y = self.l2(y)
-        return y
+#     def forward(self, x):
+#         y = F.sigmoid(self.l1(x))
+#         y = self.l2(y)
+#         return y
 
-x = Variable(np.random.randn(5,10),name='x')
-model = TwoLayerNet(100,10)
-model.plot(x)
+# x = Variable(np.random.randn(5,10),name='x')
+# model = TwoLayerNet(100,10)
+# model.plot(x)
 # %%
 np.random.seed(0)
 x = np.random.rand(100,1)
@@ -33,21 +33,20 @@ max_iter =10000
 hidden_size = 10
 
 class TwoLayerNet(Model):
+
     def __init__(self, hidden_size,out_size):
         super().__init__()
         self.l1 = L.Linear(hidden_size)
         self.l2 = L.Linear(out_size)
 
     def forward(self, x):
-        y = F.sigmoid(self.l1(x))
+        y = self.l1(x)
+        y = F.sigmoid(y)
         y = self.l2(y)
         return y
 
 model = TwoLayerNet(hidden_size,1)
-#%%
-model.params()
-for i in model.params():
-    print(i)
+
 #%%
 
 for i in range(max_iter):
@@ -58,6 +57,7 @@ for i in range(max_iter):
     loss.backward()
 
     for p in model.params():
+        
         p.data -= lr * p.grad.data
 
     if i %1000 == 0:
