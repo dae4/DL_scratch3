b1ddf94e97937b09d3d9092d0766a41d32572836
dae4 <3210m0123@gmail.com>
2021-09-03 20:32:12 +0900

    update

--- Chapter5/step52.py
+++ Chapter5/step52.py
@@ -5,7 +5,7 @@ if '__file__' in globals():
 
 import cupy as cp
 
-x= cp.arrange(6).reshape(2,3)
+x= cp.arange(6).reshape(2,3)
 print(x)
 
 y = x.sum(axis=1)
@@ -23,7 +23,7 @@ n = cp.asnumpy(c)
 assert type(n) == np.ndarray
 #%%
 x = np.array([1,2,3])
-xp = cp.get_array_modelue(x)
+xp = cp.get_array_module(x)
 assert xp == np
 
 x = cp.array([1,2,3])
@@ -38,15 +38,15 @@ from dezero import DataLoader
 from dezero.models import MLP
 import dezero.cuda
 
-max_epoch = 5 
+max_epoch = 5
 batch_size = 100
-lr = 1.0
 
 train_set = dezero.datasets.MNIST(train=True)
-train_loader = DataLoader(train_set,batch_size)
-model = MLP((1000,10))
-optimizer = optimizers.SGD(lr).setup(model)
+train_loader = DataLoader(train_set, batch_size)
+model = MLP((1000, 10))
+optimizer = optimizers.SGD().setup(model)
 
+# GPU mode
 if dezero.cuda.gpu_enable:
     train_loader.to_gpu()
     model.to_gpu()
@@ -57,15 +57,13 @@ for epoch in range(max_epoch):
 
     for x, t in train_loader:
         y = model(x)
-        loss = F.softmax_cross_entropy(y,t)
+        loss = F.softmax_cross_entropy(y, t)
         model.cleargrads()
         loss.backward()
         optimizer.update()
-
         sum_loss += float(loss.data) * len(t)
+
     elapsed_time = time.time() - start
-    print('epoch : {}'.format(epoch+1))
-    print('train loss : {:4}, time{:4f}[sec]'.format(sum_loss/len(train_set),elapsed_time))
-
-
+    print('epoch: {}, loss: {:.4f}, time: {:.4f}[sec]'.format(
+        epoch + 1, sum_loss / len(train_set), elapsed_time))
 # %%
